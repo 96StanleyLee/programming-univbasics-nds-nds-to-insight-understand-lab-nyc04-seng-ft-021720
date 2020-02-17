ee2e5d89a7fd6ba02643d55aff6eaf484e910455
Stanley Lee <sachih@live.com>
2020-02-17 05:23:05 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
    x = directors_database[0]
    i = 0 
    while i < x.length do 
-     puts x[:movies][i][:title]
+     print x[:movies][i][:title]
      i += 1 
    end 
    
